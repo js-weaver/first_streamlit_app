073af1c405c5c4c5c20f3dd730856aba1daae37e
js-weaver <126171784+js-weaver@users.noreply.github.com>
2023-02-23 16:44:42 +0200

    Update streamlit_app.py
    Added Pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,5 @@ streamlit.text('🥑🍞 Avocardo Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
-streamlit.header('\N{flexed biceps}  Build Your Muscles \N{flexed biceps} ')
-
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
