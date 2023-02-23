44ea5ee6c88b52fa97758d1a47169a57d4de4d2a
js-weaver <126171784+js-weaver@users.noreply.github.com>
2023-02-23 16:20:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My Parents New Healty Diner')
 
 streamlit.header('Breackfast Menu')
 streamlit.test('Omega 3 and Blueberry Oatmeal')
-streamlit.test('Kale, Spinach & Rocket Smoothie'')
+streamlit.test('Kale, Spinach & Rocket Smoothie')
 streamlit.test('Hard-Boiled Free-Range Egg')
