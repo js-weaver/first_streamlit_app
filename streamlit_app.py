308d6342568027e5236cfc3ec9198b827cdd1fa5
js-weaver <126171784+js-weaver@users.noreply.github.com>
2023-02-27 11:29:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,5 +27,9 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# Take the json version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output it to the screen as a table
+streamlit.dataframe(fruityvice_normalized)
 
 
