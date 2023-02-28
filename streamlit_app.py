7e1aa9ebdf6095792fffaa80818b4ef1722cf0d1
js-weaver <126171784+js-weaver@users.noreply.github.com>
2023-02-28 14:01:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,8 @@ streamlit.dataframe(fruits_to_show)
 
 def get_fruityvice_data(this_fruit_choice):
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +  this_fruit_choice)
-     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-     return(fruityvice_normalized)
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   return(fruityvice_normalized)
  
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -39,7 +39,6 @@ try:
       back_from_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
      
-
 except URLError as e:
     streamlit.error()
     
