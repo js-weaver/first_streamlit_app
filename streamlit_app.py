8d6e5dee96f781bca508b31b9e25db5fecf05105
js-weaver <126171784+js-weaver@users.noreply.github.com>
2023-02-28 15:54:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,11 +60,11 @@ streamlit.dataframe(my_data_rows)
 # Take the json version of the response and normalize it
 # output it to the screen as a table
    
-#streamlit.stop()
+streamlit.stop()
 
 #Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
-   with my_cnx_cursor() as my_cur:
+   with my_cnx.cursor() as my_cur:
       my_cur.execute("insert into fruit_load_list values ('from streamlit')")
       return "Thanks for adding " + new_fruit
      
